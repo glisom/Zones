e5d3ba05b24d012c51b13035a81159ccda0eafcf
glisom <glisom@icloud.com>
2023-07-30 15:30:14 -0500

    Initial Commit

---
+++ Zones/ZonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZonesApp.swift
+//  Zones
+//
+//  Created by Grant Isom on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ZonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
